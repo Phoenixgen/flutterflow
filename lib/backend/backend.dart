095263a7abd304d42e90df782eafc4b172477e7f
Phoenixgen <86092580+Phoenixgen@users.noreply.github.com>
2021-06-21 08:58:31 +0530

    Update backend.dart

--- lib/backend/backend.dart
+++ lib/backend/backend.dart
@@ -46,7 +46,7 @@ Future maybeCreateUser(User user) async {
     displayName: user.displayName,
     photoUrl: user.photoURL,
     uid: user.uid,
-    phoneNumber: user.phoneNumber,
+   // phoneNumber: user.phoneNumber,
     createdTime: getCurrentTimestamp,
   );
 
